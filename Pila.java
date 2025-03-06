a1a06404f713b5365eb064f7565f15b1fc163f54
Sala K 402 <salak402@salasfr.itm.edu.co>
2025-03-06 06:43:15 -0500

    first commit

---
+++ Pila.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Pila {
+    public void pila(){
+        Scanner sc = new Scanner(System.in);
+        Stack<Integer> pila = new Stack();
+        Utils utils = new Utils();
+    }
+}
